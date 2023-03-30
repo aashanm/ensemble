b520ad64b90f052742d69b94fc86a0c8802fc2d1
Aashan M <aashan.meiyappan@uwaterloo.ca>
2023-03-30 12:51:34 -0400

    Added multi-channel output code

--- heartbeat_sound_hardcoded.py
+++ heartbeat_sound_hardcoded.py
@@ -1,7 +1,9 @@
 import serial
 from pydub import AudioSegment
-from pydub.playback import play
-
+import sounddevice as sd
+import soundfile as sf
+import random
+import numpy as np
 
 user_heartbeats = []
 single_beat_repetitions = 10
@@ -9,7 +11,15 @@ multiple_beat_repetitions = 5
 bpm = [110, 75, 80, 48, 55]       
 track = AudioSegment.empty()
 single_heartbeat = AudioSegment.from_file("Downloads/single-heartbeat-sound-effect_qiBGgufL.mp3")
-violin = AudioSegment.from_file("Downloads/violin.mp3")
+
+frequencies = [240, 280, 330, 540, 600, 620, 910, 1210, 1360, 1460, 1760, 2150, 2430, 2560, 2710, 3000, 3190, 3710, 3830, 3920]
+volumes = [35, 35, 30, 35, 45, 45, 45, 45, 45, 40, 45, 45, 45, 45, 55, 45, 50, 45, 50, 55]
+freq_vol_pairs = dict(zip(frequencies, volumes))
+freq_duration_sec = 1.5
+sampling_freq = 44100
+
+frequency_speaker_id = 0
+heartbeat_speaker_id = 2
 
 def open_serial_port(port_name):
     ser = serial.Serial(port=port_name, baudrate=9600)
@@ -21,7 +31,6 @@ try:
 except:
     ser = open_serial_port('/dev/cu.usbmodem101')
 '''
-
 
 for i in range(1000):
     # line = ser.readline()      
@@ -47,11 +56,19 @@ for i in range(1000):
                 user_heartbeats.append(user_heartbeats[i-1].overlay(((single_heartbeat) + AudioSegment.silent(duration=delays[i] * 1000)) 
                     * multiple_beat_repetitions, position=delays[i] * 1000))
                 
-        play((user_heartbeats[0] + 10).overlay(violin - 8))
-        play((user_heartbeats[1] + 10).overlay(violin - 8))
-        play((user_heartbeats[2] + 10).overlay(violin - 8))
-        play((user_heartbeats[3] + 10).overlay(violin - 8))
-        play((user_heartbeats[4] + 10).overlay(violin - 8))
+        grouped_frequencies = [frequencies[i:i+4] for i in range(0, len(frequencies), 4)]
+        selected_frequencies = [random.choice(group) for group in grouped_frequencies]
+                
+        for i, clip in enumerate(user_heartbeats):
+            clip.export("temp.wav", format="wav")
+            audio_data, sample_rate = sf.read("temp.wav", dtype='float32')
+            sd.play(audio_data, sample_rate, device=heartbeat_speaker_id)
+            sd.wait()
+
+            t = np.linspace(0.0, freq_duration_sec, int(freq_duration_sec * sampling_freq), endpoint=False)
+            waveform = (freq_vol_pairs[selected_frequencies[i]] - 100) * np.sin(2.0 * np.pi * selected_frequencies[i] * t)
+            sd.play(waveform, sampling_freq, device=frequency_speaker_id)
+            sd.wait()
 
         '''
         for i in range(5):
